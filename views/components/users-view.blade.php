e37b82665071deb0d86df9d7a6dd0c9a7d992c15
Taha Paksu <tpaksu@gmail.com>
2020-07-16 00:16:32 +0300

    Playing with style

--- views/components/users-view.blade.php
+++ views/components/users-view.blade.php
@@ -1,9 +1,9 @@
 <div class="container mx-auto">
-    <div class="flex pb-4">
-        <div class="w-1/2 items-end flex">
+    <div class="flex flex-col px-4 sm:px-0 sm:flex-row sm:pb-4">
+        <div class="sm:w-1/2 items-end flex">
             <h2 class="text-2xl font-semibold">@lang("User Management")</h2>
         </div>
-        <div class="w-1/2 text-right">
+        <div class="sm:w-1/2 mt-3 sm:mt-0 sm:text-right">
             <button class="inline-block rounded bg-blue-700 text-white hover:bg-blue-800 py-2 px-3 shadow-outline-gray"
                     wire:click="$emit('showModal', 'Create new User', 'users-ui::users-form')">
                 @lang("Create new User")
