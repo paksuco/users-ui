35971038e487668ba7618a4df59225fbbc27e103
Taha Paksu <taha.paksu@edata.com.tr>
2020-08-07 16:25:33 +0300

    Remove outer padding

--- views/components/users-view.blade.php
+++ views/components/users-view.blade.php
@@ -1,5 +1,5 @@
-<div class="container p-4">
-    <div class="flex flex-col px-4 sm:px-0 sm:flex-row sm:pb-4">
+<div class="md:p-4">
+    <div class="flex flex-col sm:px-0 sm:flex-row sm:pb-4">
         <div class="sm:w-1/2 items-end flex">
             <h2 class="text-2xl font-semibold">@lang("User Management")</h2>
         </div>
